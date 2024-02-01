e03ede2674379a14c7b6b2b8c9a6a70515bd9219
Darosss <daroing12@gmail.com>
2024-02-01 17:46:33 +0100

    add better enemy related endpoints for debug

--- src/main/java/com/example/battle/BattleController.java
+++ src/main/java/com/example/battle/BattleController.java
@@ -7,11 +7,11 @@ import com.example.characters.Character;
 import com.example.characters.CharacterService;
 import com.example.enemies.Enemy;
 import com.example.enemies.EnemyService;
+import com.example.utils.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +22,13 @@ public class BattleController implements SecuredRestController {
     private final AuthenticationFacade authenticationFacade;
     private CharacterService characterService;
     private EnemyService enemyService;
+
+
+    public enum EnemyLevelChoice {
+        NOOB, WEAKER, EQUAL, FAIR, STRONGER, HARDEST
+    }
+
+    private record IntRange(int min, int max){}
 
     @Autowired
     BattleController(BattleManagerService battleManagerService,
@@ -36,13 +43,35 @@ public class BattleController implements SecuredRestController {
         this.authenticationFacade = authenticationFacade;
     }
 
+    private IntRange getEnemyLevelsRange(EnemyLevelChoice strength, int characterLevel){
+        return switch (strength){
+            case NOOB -> new IntRange(Math.max(1,characterLevel-40), characterLevel-10 );
+            case WEAKER -> new IntRange(Math.max(1, characterLevel-10), Math.max(1, characterLevel-2) );
+            case EQUAL -> new IntRange(characterLevel, characterLevel);
+            case FAIR -> new IntRange(Math.max(1, characterLevel-1),Math.max(1, characterLevel+1) );
+            case STRONGER -> new IntRange(characterLevel+5, characterLevel+10);
+            case HARDEST -> new IntRange(characterLevel+10, characterLevel+50);
+        };
+    }
 
+    private double getEnemyStatsMultipler(Enemy.EnemyType type){
+        return switch (type){
+            case EASY -> 0.5;
+            case NORMAL -> 1;
+            case MEDIUM -> 1.2;
+            case HARD -> 2;
+            case IMPOSSIBLE -> 3;
+        };
+    }
     @PostMapping("/debug/attack/{enemyType}")
     public FightReport DebugAttack(
             @PathVariable Enemy.EnemyType enemyType
-            ) throws Exception {
+    ) throws Exception {
         Optional<Character> foundCharacter = this.characterService.findOneMainCharacterByUserId(this.authenticationFacade.getJwtTokenPayload().id());
-        List<Enemy> enemies =  List.of(this.enemyService.createRandomEnemy(enemyType));
+        int randomLevel = RandomUtils.getRandomValueWithinRange(1,100);
+        List<Enemy> enemies =  List.of(
+                this.enemyService.createRandomEnemyBasedOnLevel(randomLevel,enemyType,2
+                ));
         if(foundCharacter.isPresent()){
             Character characterInst = foundCharacter.get();
             FightReport report =  this.battleManagerService.performNormalFight(List.of(characterInst), enemies);
@@ -51,23 +80,51 @@ public class BattleController implements SecuredRestController {
             this.characterService.update(characterInst);
             return report;
         }
-       return null;
+        return null;
+    }
+    @PostMapping("/debug/attack/{enemyLevel}/{enemyType}")
+    public FightReport DebugAttackWithEnemyStrength(
+            @PathVariable EnemyLevelChoice enemyLevel,
+            @PathVariable Enemy.EnemyType enemyType
+    ) throws Exception {
+        Optional<Character> foundCharacter = this.characterService.findOneMainCharacterByUserId(this.authenticationFacade.getJwtTokenPayload().id());
+
+        if(foundCharacter.isPresent()){
+            Character characterInst = foundCharacter.get();
+            IntRange enemyRange = getEnemyLevelsRange(enemyLevel, characterInst.getLevel());
+            List<Enemy> enemies =  List.of(this.enemyService.createRandomEnemyBasedOnLevel(
+                    RandomUtils.getRandomValueWithinRange(enemyRange.min(), enemyRange.max()), enemyType, this.getEnemyStatsMultipler(enemyType)
+            ));
+
+            FightReport report =  this.battleManagerService.performNormalFight(List.of(characterInst), enemies);
+
+            characterInst.gainExperience(report.getGainedExperience());
+            this.characterService.update(characterInst);
+            return report;
+        }
+        return null;
     }
 
     @PostMapping("/debug/attack-many-characters")
     public FightReport DebugAttackMultipleCharacters() throws Exception {
         List<Character> foundCharacters = this.characterService.findUserCharacters(this.authenticationFacade.getJwtTokenPayload().id());
-        List<Enemy> enemies =  List.of(this.enemyService.createRandomEnemy(Enemy.EnemyType.NORMAL));
+        int randomLevel = RandomUtils.getRandomValueWithinRange(1,100);
+        List<Enemy> enemies =  List.of(this.enemyService.createRandomEnemyBasedOnLevel(
+                randomLevel,Enemy.EnemyType.NORMAL, 2));
+
         if(!foundCharacters.isEmpty()){
             return this.battleManagerService.performNormalFight(foundCharacters, enemies);
-
         }
         return null;
     }
     @PostMapping("/debug/many-vs-many")
     public FightReport DebugManyVsMany() throws Exception {
         List<Character> foundCharacters = this.characterService.findUserCharacters(this.authenticationFacade.getJwtTokenPayload().id());
-        List<Enemy> enemies =  List.of(this.enemyService.createRandomEnemy(Enemy.EnemyType.NORMAL), this.enemyService.createRandomEnemy(Enemy.EnemyType.NORMAL), this.enemyService.createRandomEnemy(Enemy.EnemyType.NORMAL));
+        List<Enemy> enemies =  List.of(
+            this.enemyService.createRandomEnemyBasedOnLevel(20,Enemy.EnemyType.IMPOSSIBLE,2),
+            this.enemyService.createRandomEnemyBasedOnLevel(25,Enemy.EnemyType.NORMAL, 1.2),
+            this.enemyService.createRandomEnemyBasedOnLevel(25,Enemy.EnemyType.EASY, 0.8)
+        );
         if(!foundCharacters.isEmpty()){
             return this.battleManagerService.performNormalFight(foundCharacters, enemies);
         }
--- src/main/java/com/example/enemies/EnemyService.java
+++ src/main/java/com/example/enemies/EnemyService.java
@@ -2,7 +2,6 @@ package com.example.enemies;
 
 import com.example.statistics.AdditionalStatisticsNamesEnum;
 import com.example.statistics.BaseStatisticsNamesEnum;
-import com.example.utils.RandomUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -10,72 +9,30 @@ import java.util.Map;
 @Service
 public class EnemyService {
 
-    public Enemy createRandomEnemy(Enemy.EnemyType enemyType){
-        return new Enemy(
-            "Some enemy", RandomUtils.getRandomValueWithinRange(5,40),
-                enemyType,
+    public Enemy createRandomEnemy(int level, Enemy.EnemyType enemyType,
+                                   Map<BaseStatisticsNamesEnum, Integer> baseStatistics,
+                                   Map<AdditionalStatisticsNamesEnum, Integer> additionalStatistics
+                                   ){
+        return new Enemy("Some enemy", level,enemyType, baseStatistics, additionalStatistics);
+    }
+
+
+    public Enemy createRandomEnemyBasedOnLevel(int level, Enemy.EnemyType type, double statsMultipler){
+        int multipledStat = (int) (level * statsMultipler);
+        return this.createRandomEnemy(level, type,
                 Map.of(
-                        BaseStatisticsNamesEnum.STRENGTH, 20,
-                        BaseStatisticsNamesEnum.DEXTERITY, 20,
-                        BaseStatisticsNamesEnum.CHARISMA, 20,
-                        BaseStatisticsNamesEnum.CONSTITUTION, 20,
-                        BaseStatisticsNamesEnum.INTELLIGENCE, 20,
-                        BaseStatisticsNamesEnum.LUCK, 20
+                        BaseStatisticsNamesEnum.STRENGTH, multipledStat,
+                        BaseStatisticsNamesEnum.DEXTERITY, multipledStat,
+                        BaseStatisticsNamesEnum.CHARISMA, multipledStat,
+                        BaseStatisticsNamesEnum.CONSTITUTION, multipledStat,
+                        BaseStatisticsNamesEnum.INTELLIGENCE, multipledStat,
+                        BaseStatisticsNamesEnum.LUCK, multipledStat
                 ),
                 Map.of(
-                        AdditionalStatisticsNamesEnum.INITIATIVE, 300,
-                        AdditionalStatisticsNamesEnum.MAX_HEALTH, 100
+                        AdditionalStatisticsNamesEnum.INITIATIVE, multipledStat,
+                        AdditionalStatisticsNamesEnum.MAX_HEALTH, multipledStat
                 )
         );
     }
 }
 
-//public class HelloWorld {
-//    public static void calculateExperienceFromEnemy(int playerLevel, int enemyLevel, double typeMod) {
-//        int baseExperience = 25;
-//        double levelDifference = enemyLevel - playerLevel;
-//        double enemyDifFactor = levelDifference >= 0 ?
-//                Math.max(1, levelDifference + (4/100 * levelDifference) ) :
-//                Math.min(-1, (enemyLevel - levelDifference)  * baseExperience );
-//        double scalingFactor =
-//                baseExperience * (Math.pow(enemyLevel+(1.2*enemyLevel), 0.3))
-//                        +  (baseExperience * enemyDifFactor);
-//
-//
-//        long experience = (long) Math.max(-10000, (baseExperience * scalingFactor * typeMod));
-//
-//        System.out.println("scaliNg factor: "+scalingFactor+" | experience: " + experience + " | enemy level: " + enemyLevel+ " | player lvl: " + playerLevel + " | diff " + enemyDifFactor +" b + d: " + baseExperience  * enemyDifFactor );
-//
-//    }
-//    public static long calculateExpToLevelUp(int level) {
-//        double levelDecimalFactor = (double)level / 30;
-//
-//        long neededExp = (long) (100 * (Math.pow(level+(1.2*level), (1.4 + levelDecimalFactor))));
-//
-//        return neededExp;
-//    }
-//    public static void main(String []args) {
-//
-//        for(int i = 25; i < 27; i++) {
-//            long diff = calculateExpToLevelUp(i) - calculateExpToLevelUp(i-1);
-//            System.out.println(i + ": "+ calculateExpToLevelUp(i-1) + " -> " + calculateExpToLevelUp(i)+ " diff: -> " + diff);
-//
-//            calculateExperienceFromEnemy(i, i-24, 1);
-//            calculateExperienceFromEnemy(i, i-20, 1);
-//            calculateExperienceFromEnemy(i, i-15, 1);
-//            calculateExperienceFromEnemy(i, i-10, 1);
-//            calculateExperienceFromEnemy(i, i-6, 1);
-//            calculateExperienceFromEnemy(i, i, 1);
-//            calculateExperienceFromEnemy(i, i+6, 1);
-//            calculateExperienceFromEnemy(i, i+15, 1);
-//
-//        }
-//
-//
-//
-//    }
-//
-//
-//
-//
-//}
