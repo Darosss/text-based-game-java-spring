1b36f0ed0f29312202166e67f5c6020aa7ef8c9b
Darosss <daroing12@gmail.com>
2024-02-16 12:54:57 +0100

    feat: add proper usage of morphia-config.properties

--- src/main/java/com/example/configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/configuration/ApplicationConfiguration.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+import dev.morphia.config.MorphiaConfig;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +24,9 @@ class ApplicationConfiguration {
     @Bean
     @Scope("singleton")
     public Datastore datastore(MongoClient mongoClient) {
-        this.datastore = Morphia.createDatastore(mongoClient, DB_NAME);
 
+        MorphiaConfig config = MorphiaConfig.load().database(DB_NAME);
+        this.datastore = Morphia.createDatastore(mongoClient, config);
         return this.datastore;
     }
 
