103b981a5e72007f0de35afc938a81e7f5a5b757
Darosss <daroing12@gmail.com>
2024-02-18 22:22:15 +0100

    feat(debug): add and improve item debug endpoints

--- src/main/java/com/example/items/ItemDebugDataGeneratorController.java
+++ src/main/java/com/example/items/ItemDebugDataGeneratorController.java
@@ -1,20 +1,32 @@
 package com.example.items;
 
+import com.example.auth.AuthenticationFacade;
 import com.example.auth.SecuredRestController;
+import com.example.users.User;
+import com.example.users.UserService;
+import com.example.users.inventory.Inventory;
+import com.example.users.inventory.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 //CONTROLLER FOR DEBUGGING
 //EASILY ADD RANDOM ITEMS
 //ALL METHODS PUBLIC FOR EASY DEBUG ACCESS
 @RestController
 public class ItemDebugDataGeneratorController implements SecuredRestController {
-    private ItemService service;
+    private final ItemService service;
+    private final UserService userService;
+    private final InventoryService inventoryService;
+    private final AuthenticationFacade authenticationFacade;
 
     @Autowired
-    public ItemDebugDataGeneratorController(ItemService service) {
+    public ItemDebugDataGeneratorController(ItemService service, UserService userService, InventoryService inventoryService, AuthenticationFacade authenticationFacade) {
         this.service = service;
+        this.userService = userService;
+        this.inventoryService = inventoryService;
+        this.authenticationFacade = authenticationFacade;
     }
 
     @GetMapping("items/")
@@ -23,18 +35,40 @@ public class ItemDebugDataGeneratorController implements SecuredRestController {
     }
 
     @PostMapping("items/debug/create-with-random-data/{countOfItems}/")
-    public List<Item> generateItemWithRandomData(@PathVariable int countOfItems){
-        return service.create(ItemUtils.generateRandomItems(countOfItems));
+    public List<Item> generateItemWithRandomData(@PathVariable int countOfItems) throws Exception {
+        String userId = this.authenticationFacade.getJwtTokenPayload().id();
+        Optional<User> user = this.userService.findOneById(userId);
+        Inventory inventory = this.inventoryService.getUserInventory(userId);
+        if(user.isPresent()) {
+            List<Item> items = this.service.create(ItemUtils.generateRandomItems(countOfItems));
+            for (Item item : items) {
+                inventory.addItem(item);
+            }
+            this.inventoryService.update(inventory);
+
+            return items;
+        }
+        return null;
     }
 
     @PostMapping("items/debug/createItem/{level}/{type}")
     public Item generateItemWithLevelAndType(
             @PathVariable int level,
             @PathVariable ItemTypeEnum type
-    ){
-        return service.create(ItemUtils.generateRandomItemWithoutBaseStats(
-                "Item custom level", level, type
-        ));
+    ) throws Exception {
+        String userId = this.authenticationFacade.getJwtTokenPayload().id();
+        Optional<User> user = this.userService.findOneById(userId);
+        Inventory inventory = this.inventoryService.getUserInventory(userId);
+        if(user.isPresent()) {
+            Item item = service.create(ItemUtils.generateRandomItemWithoutBaseStats(
+                    "Item custom level", level, type));
+            inventory.addItem(item);
+            inventoryService.update(inventory);
+
+            return item;
+        }
+
+        return null;
     }
 
     @DeleteMapping("items/debug/delete-all")
