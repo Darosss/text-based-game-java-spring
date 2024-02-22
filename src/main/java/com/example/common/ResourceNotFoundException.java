aa05489609285fdf26030f22c6befa5180b06a86
Darosss <daroing12@gmail.com>
2024-02-22 15:37:50 +0100

    feat: add ResourceNotFoundException

---
+++ src/main/java/com/example/common/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.common;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resourceType, String resourceId) {
+        super("Resource of type " + resourceType + " with ID " + resourceId + " not found");
+    }
+    public ResourceNotFoundException(String customMessage) {
+        super(customMessage);
+    }
+
+}
