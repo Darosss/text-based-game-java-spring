75bdf2736f1be4237bde97f1c83c8b37d666c226
Darosss <daroing12@gmail.com>
2024-02-22 15:39:57 +0100

    feat(getLoggedUserDetails): change to throw ResourceNotFoundException

--- src/main/java/com/example/auth/LoggedUserUtils.java
+++ src/main/java/com/example/auth/LoggedUserUtils.java
@@ -1,5 +1,6 @@
 package com.example.auth;
 
+import com.example.common.ResourceNotFoundException;
 import com.example.users.User;
 import com.example.users.UserService;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -9,12 +10,11 @@ import java.util.Optional;
 public class LoggedUserUtils {
 
     private LoggedUserUtils() {
-        // Private constructor to prevent instantiation
     }
 
     public static User getLoggedUserDetails(AuthenticationFacade authenticationFacade, UserService userService) throws Exception {
         String userId = authenticationFacade.getJwtTokenPayload().id();
         Optional<User> foundUser = userService.findOneById(userId);
-        return foundUser.orElseThrow(() -> new EmptyResultDataAccessException("User not found", 1));
+        return foundUser.orElseThrow(() -> new ResourceNotFoundException("Not found your user details. Please try again later"));
     }
 }
