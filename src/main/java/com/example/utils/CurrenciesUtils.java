3f62b6f3793e490e19d461a7561e44ac7d777e42
Darosss <daroing12@gmail.com>
2024-03-04 23:14:16 +0100

    refactor(currencies): replace getEnemyTypeBonus with property from enum - EnemyType: add bonusGold property in enum - CurrenciesUtils: remove getEnemyTypeBonus and use property from EnemyType enum

--- src/main/java/com/example/utils/CurrenciesUtils.java
+++ src/main/java/com/example/utils/CurrenciesUtils.java
@@ -12,7 +12,7 @@ public class CurrenciesUtils {
 
 
     public static long calculateGoldFromEnemy(int enemyLevel, EnemyType enemyType, boolean isAlive) {
-        double enemyTypeBonus = getEnemyTypeBonus(enemyType);
+        double enemyTypeBonus = enemyType.getBonusGold();
 
         long gold = (long) Math.max(1, (enemyLevel * BASE_GOLD_FOR_ENEMY_LEVEL) * enemyTypeBonus);
 
@@ -25,14 +25,5 @@ public class CurrenciesUtils {
 
 
 
-    private static double getEnemyTypeBonus(EnemyType enemyType) {
-        return switch (enemyType) {
-            case COMMON -> 1.0;
-            case UNCOMMON -> 1.5;
-            case RARE -> 2.0;
-            case EPIC -> 2.5;
-            case BOSS -> 3.0;
-            case ANCESTOR -> 5.0;
-        };
-    }
+
 }
