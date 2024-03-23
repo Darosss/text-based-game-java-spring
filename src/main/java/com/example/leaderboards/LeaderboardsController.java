5ba5d22f90b70d6a0a51c491257407cc2aa4d6ca
Darosss <daroing12@gmail.com>
2024-03-23 21:41:14 +0100

    feat(Leaderboards): add get category route in controller

--- src/main/java/com/example/leaderboards/LeaderboardsController.java
+++ src/main/java/com/example/leaderboards/LeaderboardsController.java
@@ -2,13 +2,16 @@ package com.example.leaderboards;
 
 import com.example.auth.SecuredRestController;
 import com.example.response.CustomResponse;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController("leaderboards")
 @RequestMapping("leaderboards")
@@ -21,7 +24,16 @@ public class LeaderboardsController implements SecuredRestController {
     }
 
     @GetMapping("/next-refresh-time")
-    public CustomResponse<LocalDateTime> findYourMercenaries() throws Exception {
+    public CustomResponse<LocalDateTime> getNextRefreshTime()  {
         return new CustomResponse<>(HttpStatus.OK,this.service.getNextRefreshAt());
     }
+    @GetMapping("/category/{category}")
+    public CustomResponse<Leaderboard> findLeaderboardByCategory(
+            @PathVariable Leaderboard.LeaderboardCategory category
+    ) throws BadRequestException {
+        Optional<Leaderboard> leaderboard = this.service.findOneLeaderboardByCategory(category);
+        return leaderboard.map((board)->new CustomResponse<>(HttpStatus.OK, board))
+                .orElseThrow(()->new BadRequestException("Not found leaderboard with category: "+category));
+    }
+
 }
