07e1dc918167d2d948db3f69cde75031b919b7b7
darosss <darosss12@proton.me>
2025-06-28 11:19:41 +0200

    feat(characters/equipment): add UseConsumableItemResult to Equipment interface

--- src/main/java/com/textbasedgame/characters/equipment/Equipment.java
+++ src/main/java/com/textbasedgame/characters/equipment/Equipment.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface Equipment {
     record EquipItemResult(boolean success, String message) {}
     record UnEquipItemResult(boolean success, String message, Optional<Item> item) {}
+    record UseConsumableItemResult(boolean success, String message, Optional<Integer> newHealth) {}
 }
